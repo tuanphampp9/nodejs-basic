7fee28aab04a37202d5932046c7a594a227c96f6
tuanpham <tuanphampp9@gmail.com>
2023-06-24 21:32:24 +0700

    config model MVC

---
+++ src/route/web.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import homeController from '../controller/homeController'
+let router = express.Router();
+
+const initWebRoute = (app) => {
+    router.get('/', homeController.getHomepage)
+
+    router.get('/about', (req, res) => {
+        res.send('Tuan day, hello');
+    })
+    return app.use('/', router);
+}
+
+export default initWebRoute;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,12 @@
 import express from 'express'
 import configViewEngine from './configs/viewEngine';
+import initWebRoute from './route/web';
 import 'dotenv/config'
 const path = require('path');
 const app = express()
 const port = process.env.PORT;
 configViewEngine(app);
-app.get('/', (req, res) => {
-    res.render('index.ejs');
-})
-
-app.get('/about', (req, res) => {
-    res.send('Tuan day, hello');
-})
-
+initWebRoute(app);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
