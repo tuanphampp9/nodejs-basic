0a453f4e2365b13dc568eea8e62799bb492e5f02
tuanpham <tuanphampp9@gmail.com>
2023-07-01 01:05:20 +0700

    add middleware page not found and morgan logging

--- src/server.js
+++ src/server.js
@@ -2,15 +2,20 @@ import express from 'express'
 import configViewEngine from './configs/viewEngine';
 import initWebRoute from './route/web';
 import initApiRoute from './route/api';
+var morgan = require('morgan')
 import 'dotenv/config'
 const app = express()
-
+app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 const port = process.env.PORT;
 configViewEngine(app);
 initWebRoute(app);
 initApiRoute(app);
+
+app.use((req, res) => {
+    return res.render('pageNotFound.ejs');
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
